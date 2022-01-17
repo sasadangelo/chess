fbcf79d1ecc14057d1848b523f626f37dc69d6bc
Salvatore D'Angelo <sasadangelo@gmail.com>
2022-01-17 22:28:28 +0100

    Improve italian game pdf

--- pgn2tex.py
+++ pgn2tex.py
@@ -18,14 +18,12 @@ TITLE_PAGE="\maketitle\n"
 END_TITLE_PAGE="\end{titlepage}\n"
 BOLD="\\textbf"
 USE_PACKAGE_SVG="\\usepackage{svg}\n"
-#GRAPHICSPATH="\graphicspath"
 INCLUDE_GRAPHICS="\includesvg"
 
 # Parse the input arguments.
 # The program accepts only one parameter: the PGN file name.
 parser = argparse.ArgumentParser()
 parser.add_argument("pgn", help="PGN file with games and variants")
-parser.add_argument("--title", action='store', type = str)
 args = parser.parse_args()
 
 # Check if the file exists
@@ -41,15 +39,26 @@ if not os.path.isfile(args.pgn):
 # Open the PGN file
 pgn = open(args.pgn)
 
-# Load all the chapters from the PGN file
-chapters = []
+# The first page of the PFD file will contain:
+# * The title
+# * The author
+# * The publishing date
 author=""
 date=""
+title=""
+
+# Load all the chapters from the PGN file
+chapters = []
 while True:
     chapter=chess.pgn.read_game(pgn)
+    # The PGN file is divided in Chapters. In each chapter there are the following field:
+    # * Chapter title
+    # * Author
+    # * The publishing date
     if chapter is not None:
         author=chapter.headers["Annotator"]
         date=chapter.headers["UTCDate"]
+        title=chapter.headers["Event"].split(":", 2)[0]
         chapters.append(chapter)
     else: 
         break
@@ -62,38 +71,49 @@ image_count=1
 os.makedirs(filename_wo_suffix)
 with open(latex_filename, 'w') as latex_file:
     latex_file.write(ARTICLE)
-    if args.title != "":
-        latex_file.write(TITLE + "{" + args.title + "}\n")
+    if title != "":
+        latex_file.write(TITLE + "{" + title + "}\n")
         latex_file.write(AUTHOR + "{" + author + "}\n")
         latex_file.write(DATE + "{" + date + "}\n")
     latex_file.write(USE_PACKAGE_SVG)
     latex_file.write(BEGIN)
-    if args.title != "":
+    if title != "":
         latex_file.write(BEGIN_TITLE_PAGE)
         latex_file.write(TITLE_PAGE)
         latex_file.write(END_TITLE_PAGE)
     for chapter in chapters:
-        latex_file.write(SECTION + "{" + chapter.headers["Event"] + "}\n")
+        chapter_title=chapter.headers["Event"].split(":", 2)[1]
+        latex_file.write(SECTION + "{" + chapter_title + "}\n")
         latex_file.write("\n")
         if chapter.comment != "" :
-            latex_file.write(chapter.comment + "\\\\\n")
+            latex_file.write(chapter.comment.replace("\n", "\\\\"))
+            latex_file.write("\\\\\n")
             latex_file.write("\\\\\n")
         board = chapter.board()
         variations = chapter.variations
         while len(variations)>0:
             board_image_filename=filename_wo_suffix + "/" + filename_wo_suffix + "_" + str(image_count) + ".svg"
             variation = variations[0]
-            latex_file.write(INCLUDE_GRAPHICS + "[width=150pt]{" + board_image_filename + "}\n\\\\\n\\\\\n") 
+            #pprint(variation.comment,depth=2)
+            latex_file.write(INCLUDE_GRAPHICS + "[width=150pt]{" + board_image_filename + "}\n")
+            latex_file.write("\\\\\n")
+            latex_file.write("\\\\\n") 
             if board.turn:
-                latex_file.write(BOLD + "{White " + str(board.fullmove_number) + ". " + board.san(variation.move) + "}\\\\\n")
+                latex_file.write(BOLD + "{White " + str(board.fullmove_number) + ". " + board.san(variation.move) + "}")
             else:
-                latex_file.write(BOLD + "{Black " + str(board.fullmove_number) + "... " + board.san(variation.move) + "}\\\\\n")
+                latex_file.write(BOLD + "{Black " + str(board.fullmove_number) + "... " + board.san(variation.move) + "}")
+            latex_file.write("\\\\\n")
+            latex_file.write("\\\\\n")
             board.push(variation.move)
             with open(board_image_filename, "w") as board_image:
                 boardsvg = chess.svg.board(board=board)
                 board_image.write(boardsvg)
             image_count+=1
-            latex_file.write(variation.comment + "\\\\\n")
+            # print(re.sub('\[\%eval.*\]\n', '', variation.comment))
+            # print("")
+            #latex_file.write(variation.comment + "\\\\\n")
+            latex_file.write(variation.comment.replace("\n", "\\\\"))
+            latex_file.write("\\\\\n")
             latex_file.write("\\\\\n")
             variations = variation.variations
     latex_file.write(END)
